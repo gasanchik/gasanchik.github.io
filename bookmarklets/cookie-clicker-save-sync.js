2b126f114242d62000a03fee67dea094ce1e8eb4
gasanchik <46112415+gasanchik@users.noreply.github.com>
2022-11-12 15:36:12 +0000

    ...

---
+++ bookmarklets/cookie-clicker-save-sync.js
@@ -0,0 +1,52 @@
+function updateValues(spreadsheetId, range, valueInputOption, _values, callback) {
+    let values = [
+      [
+        // Cell values ...
+      ],
+      // Additional rows ...
+    ];
+    values = _values;
+    const body = {
+      values: values,
+    };
+    try {
+      gapi.client.sheets.spreadsheets.values.update({
+        spreadsheetId: spreadsheetId,
+        range: range,
+        valueInputOption: valueInputOption,
+        resource: body,
+      }).then((response) => {
+        const result = response.result;
+        console.log(`${result.updatedCells} cells updated.`);
+        if (callback) callback(response);
+      });
+    } catch (err) {
+      document.getElementById('content').innerText = err.message;
+      return;
+    }
+  }
+
+  function getValues(spreadsheetId, range, callback) {
+    try {
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: range,
+      }).then((response) => {
+        const result = response.result;
+        const numRows = result.values ? result.values.length : 0;
+        console.log(`${numRows} rows retrieved.`);
+        if (callback) callback(response);
+      });
+    } catch (err) {
+      document.getElementById('content').innerText = err.message;
+      return;
+    }
+  }
+
+//Prob some very bad coding practises, have never done something like this before lol
+window.prompt("cookie-save-sync loaded in")
+//console.log("cookie-save-sync loaded in")
+//messageElement = document.createElement('h1');
+//messageElement.setAttribute("id", "cookie-sync-messages");
+//document.getElementById('cookie-sync-messages').innerHTML = '<ol><li>html data</li></ol>';
+// <h1 style="background-color: black; color: white; font-family:Courier New,Courier,monospace; bottom: 0px; position: fixed;">Hello World!</h1>
